925421ee4a40a19629a38e31cbde8f88007dc75f
martínval11 <martinvaldez.dev@gmail.com>
2024-01-28 16:06:35 -0300

    window.location fix

--- frontend/src/app/dashboard/URLSList.tsx
+++ frontend/src/app/dashboard/URLSList.tsx
@@ -83,7 +83,11 @@ export const URLSList = ({ urls }: { urls: URLSListProps[] }) => {
       <section className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 mt-4">
         {urlList.map((url: URLSListProps) => (
           <Link
-            href={`${window?.location.origin}/${url.id}`}
+            href={
+              typeof window !== 'undefined'
+                ? `${window.location.origin}/${url.id}`
+                : `https://surlm.vercel.app/${url.id}`
+            }
             key={url.id}
             target="_blank"
             rel="noopener noreferrer"
